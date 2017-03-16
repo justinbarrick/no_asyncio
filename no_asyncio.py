623b9802091d17b8c617bcac88f01549aea8f3ab
Your Name <you@example.com>
2017-03-16 10:21:00 +0000

    Automatically convert classes with a metaclass

--- no_asyncio.py
+++ no_asyncio.py
@@ -1,51 +1,86 @@
 import asyncio
 import functools
 import uvhttp.http
+import inspect
 import ast
 
 class RewriteAST(ast.NodeTransformer):
     """
     Rewrite the AST to replace all functions that start with test_ with async
-    functions and all calls to *.do() and do() with awaits.
+    functions and all calls to the provided magic string *.magic*() and
+    magic*() with awaits.
     """
 
+    def __init__(self, magic):
+        self.magic = magic
+
+    def is_magic_call(self, node):
+        if not isinstance(node, ast.Call):
+            return False
+        elif not isinstance(node.func, ast.Name) and not isinstance(node.func, ast.Attribute):
+            return False
+        elif isinstance(node.func, ast.Name) and not node.func.id.startswith(self.magic):
+            return False
+        elif isinstance(node.func, ast.Attribute) and not node.func.attr.startswith(self.magic):
+            return False
+
+        return True
+
     def visit_FunctionDef(self, node):
-        if not node.name.startswith('test'):
+        # Ignore special functions like __init__
+        if node.name.startswith('__') and node.name.endswith('__'):
+            return node
+
+        magic = False
+
+        for child in ast.walk(node):
+            if self.is_magic_call(child):
+                magic = True
+                break
+
+        if not magic:
             return node
 
         node = ast.copy_location(ast.AsyncFunctionDef(name=node.name,
             args=node.args, body=node.body, decorator_list=node.decorator_list,
             returns=node.returns), node)
-        node = RewriteAST().visit(node)
+        node = self.generic_visit(node)
         return node
 
     def visit_Call(self, node):
-        """
-        Note: in the current implementation, awaiting on the magic function
-        won't work. Recursing the tree would be better.
-        """
-        if not isinstance(node.func, ast.Name) and not isinstance(node.func, ast.Attribute):
-            return node
-        elif isinstance(node.func, ast.Name) and node.func.id != 'do':
-            return node
-        elif isinstance(node.func, ast.Attribute) and node.func.attr != 'do':
-            return node
+        if self.is_magic_call(node):
+            return ast.copy_location(ast.Await(value=node), node)
+        return node
 
-        return ast.copy_location(ast.Await(value=node), node)
+class NoAsync(type):
+    def __new__(cls, name, bases, namespace):
+        try:
+            __importing__
+            return type.__new__(cls, name, bases, namespace)
+        except NameError:
+            pass
 
-def rewrite_ast():
-    """
-    Load the current file and rewrite globals with the new AST.
+        try:
+            inspect.stack()[1][0].f_globals['__importing__']
+            return type.__new__(cls, name, bases, namespace)
+        except KeyError:
+            pass
 
-    This should be changed to an import() function for importing modules.
-    """
-    global __name__
-    __name__ = '__notmain__'
+        # Get the file containing the class from the stack.
+        code_file = inspect.stack()[1].filename
+        tree = ast.parse(open(code_file).read())
+        tree = RewriteAST(namespace.get('magic', 'do')).visit(tree)
+        tree = ast.fix_missing_locations(tree)
 
-    tree = ast.parse(open(__file__).read())
-    tree = RewriteAST().visit(tree)
-    tree = ast.fix_missing_locations(tree)
-    exec(compile(tree, __file__, 'exec'), globals())
+        def compile_context():
+            g = globals().copy()
+            g['__importing__'] = True
+            compiled = compile(tree, code_file, 'exec')
+            exec(compiled, g)
+            return g
+
+        namespace.update(compile_context()[name].__dict__)
+        return type.__new__(cls, name, bases, namespace)
 
 def start_loop(func):
     """
@@ -58,19 +93,12 @@ def start_loop(func):
 
     return new_func
 
-@start_loop
-async def main():
-    t = Test()
-
-    # Make two requests at once.
-    task = asyncio.ensure_future(t.test_async_func())
-    task2 = asyncio.ensure_future(t.test_async_func())
-
-    await asyncio.wait([task, task2])
-
-class Test:
+class Test(metaclass=NoAsync):
     def __init__(self):
         self.session = uvhttp.http.Session(10, loop=asyncio.get_event_loop())
+
+    def test_lol(self):
+        self.do()
 
     def test_async_func(self):
         """
@@ -84,6 +112,19 @@ class Test:
     async def do(self):
         return await self.session.get(b'http://127.0.0.1/')
 
+@start_loop
+async def main():
+    t = Test()
+
+    # Make two requests at once.
+    task = asyncio.ensure_future(t.test_async_func())
+    task2 = asyncio.ensure_future(t.test_async_func())
+    task3 = asyncio.ensure_future(t.test_lol())
+
+    await asyncio.wait([task, task2, task3])
+
 if __name__ == '__main__':
-    rewrite_ast()
-    main()
+    try:
+        __importing__
+    except NameError:
+        main()
