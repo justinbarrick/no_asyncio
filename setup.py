6a1b6721cc838df00d4bd00ba1bdf987a54af486
Your Name <you@example.com>
2018-03-11 06:10:51 +0000

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='no_asyncio',
-    version='0.1',
+    version='1.0',
     description='Transparent asyncio methods',
     url='https://github.com/justinbarrick/no_asyncio',
     packages=['no_asyncio'],
